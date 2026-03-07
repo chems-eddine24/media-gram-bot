1345db97a2e105d23ff1b67e706bc15a1078465e
chems-eddine24 <obitouchiwa109@gmail.com>
2026-03-07 22:52:39 +0100

    adding the handlers and starting the bot

--- bot/main.py
+++ bot/main.py
@@ -1,23 +1,44 @@
-from telegram.ext import CommandHandler, ApplicationBuilder, CallbackQueryHandler
-from handlers.callbacks import format_choice_callback, quality_choice_callback, cancel_callback
 import logging
-from handlers.download import conv_handler
-from handlers.start import start_handler 
-from config import Settings
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    filters,
+)
+from telegram.request import HTTPXRequest
+from handlers.start import start_handler, help_handler
+from handlers.messages import url_handler, unknown_handler
+from handlers.callbacks import format_choice_callback, quality_choice_callback, cancel_callback
+from config import settings
 
-settings = Settings()
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
-    )
+)
 
-def main():
-    app = ApplicationBuilder().token(settings.TOKEN).build()
-    app.add_handler(CommandHandler('start', start_handler))
+
+def main() -> None:
+    request = HTTPXRequest(
+        connect_timeout=10,
+        read_timeout=300,    
+        write_timeout=300,   
+        media_write_timeout=300,
+        )
+    app = Application.builder().token(settings.TOKEN).request(request).build()
+
+    app.add_handler(CommandHandler("start", start_handler))
+    app.add_handler(CommandHandler("help", help_handler))
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, url_handler))
+    app.add_handler(MessageHandler(filters.COMMAND, unknown_handler))
+
     app.add_handler(CallbackQueryHandler(format_choice_callback, pattern="^audio:|^video:"))
     app.add_handler(CallbackQueryHandler(quality_choice_callback, pattern="^quality:"))
     app.add_handler(CallbackQueryHandler(cancel_callback, pattern="^cancel$"))
 
     app.run_polling()
 
-if __name__ == '__main__':    main()
+
+if __name__ == "__main__":
+    main()
